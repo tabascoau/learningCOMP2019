006227e9903449aad5b63b13925ce0b3a5abf926
AU Lok Kin <17216761@hkbuad.local>
2018-12-19 13:25:54 +0800

    Initial Commit

---
+++ api/models/Person.js
@@ -0,0 +1,44 @@
+/**
+ * Person.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    name: {
+      type: "string"
+    },
+
+    age: {
+      type: "number"
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+  getInvalidIdMsg: function (opts) {
+
+    if (typeof opts.id === "undefined" || isNaN(parseInt(opts.id)))
+      return "Person id not specified or with incorrect type.";
+
+    return null;        // falsy
+
+  },
+
+};
+
